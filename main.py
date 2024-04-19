4057795336185b631f20546a48e2c1996c377818
jmaurelli <jmaurelli@protonmail.com>
2024-04-19 05:30:51 -0400

    frakenstein updated

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        # here we are opening and printing to console
-        file_contents = f.read()
-        words = file_contents.split()
-        print(words)
+    book_path = "books/frankenstein.txt"
+    full_text = parse_text(book_path)
+    numb_words = count_words(full_text)
+    print(f"{numb_words} words in this text.")
+    numb_characters = count_characters(full_text)
+    print(numb_characters)
 
-if __name__ == "__main__": 
-    main()
+def parse_text(path):
+    with open(path) as f:
+        return f.read()
+
+def count_words(words):
+    parsed_words = words.split()
+    return len(parsed_words)
+
+def count_characters(text):
+    characters_dic = {}
+    lowered_text = text.lower()
+    for characters in lowered_text:
+        if characters not in characters_dic:
+            characters_dic[characters] = 0
+        characters_dic[characters] += 1
+    return characters_dic
+main()
